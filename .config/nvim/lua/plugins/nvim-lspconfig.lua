ab36ba9c5756e5c4a596641f151d9949fba3f5cd
Antti Kupila <akupila@gmail.com>
2023-07-01 13:02:42 +0300

    nvim: fix duplicate lsp attach

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,27 +2,8 @@ return {
   "neovim/nvim-lspconfig",
   config = function()
     local lspconfig = require("lspconfig")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-    local on_attach = function(client, bufnr)
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
-      vim.keymap.set({"i"}, "<C-k>", vim.lsp.buf.signature_help, { buffer = bufnr })
-      vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { buffer = bufnr })
-
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        desc = "Format code on save",
-        callback = function()
-          vim.lsp.buf.format()
-        end
-      })
-    end
 
     lspconfig.gopls.setup({
-      on_attach = on_attach,
       settings = {
         gopls = {
           staticcheck = true,
@@ -39,15 +20,43 @@ return {
     })
 
     lspconfig.terraformls.setup({
-      on_attach = on_attach,
     })
 
     lspconfig.tsserver.setup({
-      on_attach = on_attach,
     })
 
     lspconfig.tailwindcss.setup({
-      on_attach = on_attach,
+    })
+
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        local opts = { buffer = ev.buf }
+
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set({'n', 'i'}, '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = ev.buf,
+          callback = function()
+              vim.lsp.buf.format { async = false }
+          end
+        })
+
+      end,
     })
   end
 }
